481d6186a5d069a7f695fa04246968612342c46c
paval <paval@aqua.dtu.dk>
2022-03-10 09:50:08 +0100

    changes to sensitivity plots

--- sensitivityanalysis/sensitivityPlots.m
+++ sensitivityanalysis/sensitivityPlots.m
@@ -124,7 +124,7 @@ ylabel('\mu g C m^{-2} d^{-1} \mu m^{-1}')
 legend('\epsilon=10^{-4}','\epsilon=10^{-5}','\epsilon=10^{-6}','\epsilon=10^{-7}','location','w')
 set(gca,'FontSize',14)
 
-print('./plots/flux_epsilon.png', '-dpng', '-r400')
+%print('./plots/flux_epsilon.png', '-dpng', '-r400')
 %% alpha
 
 
@@ -141,11 +141,11 @@ ylabel('\mu g C m^{-2} d^{-1}\mu m^{-1}')
 legend('\alpha = 0.1','\alpha = 0.3','\alpha = 0.5','\alpha = 0.7','location','w')
 set(gca,'FontSize',14)
 
-print('./plots/flux_alpha.png', '-dpng', '-r400')
+%print('./plots/flux_alpha.png', '-dpng', '-r400')
 
 %% a
 
-figure('Position', [300, 300, 700, 425])
+%figure('Position', [300, 300, 700, 425])
 semilogx(x5,exportDT_x(8:10,:),'LineWidth',1.5)
 hold on
 semilogx(x4,exportDT_x(11:15,:),'LineWidth',1.5)
@@ -164,7 +164,7 @@ ylabel('\mu g C m^{-2} d^{-1}\mu m^{-1}')
 legend('a = 1.6','a = 1.7','a = 1.8','a = 1.9','a = 2.0','a = 2.1','a = 2.2','a = 2.3','location','sw')
 set(gca,'FontSize',14)
 
-print('./plots/flux_a.png', '-dpng', '-r400')
+%print('./plots/flux_a.png', '-dpng', '-r400')
 
 %%
 load('m.mat')
@@ -273,7 +273,7 @@ slope  = @(x,b)   1E-4*x.^(-b);
 monterey = @(x) 0.027*x.^(-2.965);
 
 
-figure('Position', [300, 300, 700, 425])
+%figure('Position', [300, 300, 700, 425])
 loglog(2*x4*1E-4,NNN(1:4,:), 'LineWidth',1.5)
 hold on
 plot(2*x4*1E-4,slope(2*x4*1E-4,2))
@@ -284,9 +284,9 @@ ylabel('Number spectrum [# cm^{-4}]')
 legend('\epsilon=10^{-4}','\epsilon=10^{-5}','\epsilon=10^{-6}','\epsilon=10^{-7}','slope=-2','slope=-4','Location','SouthWest')
 set(gca,'FontSize',16)
 
-print('./plots/spectrum_epsilon.png', '-dpng', '-r400')
+%print('./plots/spectrum_epsilon.png', '-dpng', '-r400')
 
-figure('Position', [300, 300, 700, 425])
+%figure('Position', [300, 300, 700, 425])
 loglog(2*x4*1E-4,NNN(5:8,:), 'LineWidth',1.5)
 hold on
 plot(2*x4*1E-4,slope(2*x4*1E-4,2))
@@ -297,9 +297,9 @@ ylabel('Number spectrum [# cm^{-4}]')
 legend('\alpha = 0.1','\alpha = 0.3','\alpha = 0.5','\alpha = 0.7','slope=-2','slope=-4','Location','SouthWest')
 set(gca,'FontSize',16)
 
-print('./plots/spectrum_alpha.png', '-dpng', '-r400')
+%print('./plots/spectrum_alpha.png', '-dpng', '-r400')
 
-figure('Position', [300, 300, 700, 425])
+%figure('Position', [300, 300, 700, 425])
 loglog(2*x5*1E-4,NNN(9:11,:), 'LineWidth',1.5)
 hold on
 loglog(2*x4*1E-4,NNN(12:16,:), 'LineWidth',1.5)
@@ -312,9 +312,112 @@ ylabel('Number spectrum [# cm^{-4}]')
 legend('a = 1.6','a = 1.7','a = 1.8','a = 1.9','a = 2.0','a = 2.1','a = 2.2','a = 2.3','slope=-2','slope=-4','Location','SouthWest')
 set(gca,'FontSize',16)
 
-print('./plots/spectrum_a.png', '-dpng', '-r400')
+%print('./plots/spectrum_a.png', '-dpng', '-r400')
+%%
 
+figure('Position', [0, 0, 900, 750])
+t = tiledlayout(3,2);
+t.TileSpacing = 'compact';
+t.Padding = 'tight';
+nexttile
 
+semilogx(x4*2*1E-4,exportDT_x(1:2,:),'LineWidth',1.5)
+hold on
+semilogx(x4*2*1E-4,exportDT_x(4,:),'LineWidth',1.5)
+semilogx(x4*2*1E-4,exportDT_x(3,:),'LineWidth',1.5)
+title('Export flux')
+% text(0.01, 0.95, 'Export flux [mgC/m^2/d]' ,'Units','normalized')
+% text(0.01, 0.9,['\epsilon = 10^{-4}: ', num2str(round(exportFlux(1)))],'Units','normalized')
+% text(0.01, 0.85, ['\epsilon = 10^{-5}: ', num2str(round(exportFlux(2)))],'Units','normalized')
+% text(0.01, 0.8, ['\epsilon = 10^{-6}: ', num2str(round(exportFlux(4)))],'Units','normalized')
+% text(0.01, 0.75, ['\epsilon = 10^{-7}: ', num2str(round(exportFlux(3)))],'Units','normalized')
+%xlabel('radius [\mu m]')
+ylabel('\mu g C m^{-2} d^{-1} \mu m^{-1}') 
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+%legend('\epsilon=10^{-4}','\epsilon=10^{-5}','\epsilon=10^{-6}','\epsilon=10^{-7}','location','northEast')
+set(gca,'FontSize',12)
 
+nexttile
 
+loglog(2*x4*1E-4,NNN(1:4,:), 'LineWidth',1.5)
+hold on
+plot(2*x4*1E-4,slope(2*x4*1E-4,2),'b--','LineWidth',1.5)
+plot(2*x4*1E-4,slope(2*x4*1E-4,4)*1000,'k--','LineWidth',1.5)
+title('Particle size spectrum')
+%xlabel('Particle Diameter [cm]')
+ylabel('Number spectrum [# cm^{-4}]')
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+legend('\epsilon=10^{-4}','\epsilon=10^{-5}','\epsilon=10^{-6}','\epsilon=10^{-7}','slope=-2','slope=-4','Location','EastOutside')
+% set(gca,'FontSize',12, 'YaxisLocation','right')
+set(gca,'FontSize',12)
 
+nexttile
+
+semilogx(x4*2*1E-4,exportDT_x(4:7,:),'LineWidth',1.5)
+%title('export flux')
+% text(0.01, 0.95, 'Export flux [mgC/m^2/d]' ,'Units','normalized')
+% text(0.01, 0.9,['\alpha = 0.1: ', num2str(round(exportFlux(4)))],'Units','normalized')
+% text(0.01, 0.85, ['\alpha = 0.3: ', num2str(round(exportFlux(5)))],'Units','normalized')
+% text(0.01, 0.8, ['\alpha = 0.5: ', num2str(round(exportFlux(6)))],'Units','normalized')
+% text(0.01, 0.75, ['\alpha = 0.7: ', num2str(round(exportFlux(7)))],'Units','normalized')
+%xlabel('radius [\mu m]')
+ylabel('\mu g C m^{-2} d^{-1}\mu m^{-1}') 
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+%legend('\alpha = 0.1','\alpha = 0.3','\alpha = 0.5','\alpha = 0.7','location','NorthEast')
+set(gca,'FontSize',12)
+
+nexttile
+
+loglog(2*x4*1E-4,NNN(5:8,:), 'LineWidth',1.5)
+hold on
+plot(2*x4*1E-4,slope(2*x4*1E-4,2),'b--','LineWidth',1.5)
+plot(2*x4*1E-4,slope(2*x4*1E-4,4)*100,'k--','LineWidth',1.5)
+%title('Particle size spectrum')
+%xlabel('Particle Diameter [cm]')
+ylabel('Number spectrum [# cm^{-4}]')
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+legend('\alpha = 0.1','\alpha = 0.3','\alpha = 0.5','\alpha = 0.7','slope=-2','slope=-4','Location','EastOutside')
+% set(gca,'FontSize',12, 'YaxisLocation','right')
+set(gca,'FontSize',12)
+
+nexttile
+
+semilogx(x5*2*1E-4,exportDT_x(8:10,:),'LineWidth',1.5)
+hold on
+semilogx(x4*2*1E-4,exportDT_x(11:15,:),'LineWidth',1.5)
+%title('export flux')
+% text(0.01, 0.95, 'Export flux [mgC/m^2/d]' ,'Units','normalized')
+% text(0.01, 0.9,['a = 1.6: ', num2str(round(exportFlux(8)))],'Units','normalized')
+% text(0.01, 0.9,['a = 1.7: ', num2str(round(exportFlux(8)))],'Units','normalized')
+% text(0.01, 0.85,['a = 1.8: ', num2str(round(exportFlux(9)))],'Units','normalized')
+% text(0.01, 0.8, ['a = 1.9: ', num2str(round(exportFlux(10)))],'Units','normalized')
+% text(0.01, 0.75, ['a = 2.0: ', num2str(round(exportFlux(11)))],'Units','normalized')
+% text(0.01, 0.7, ['a = 2.1: ', num2str(round(exportFlux(12)))],'Units','normalized')
+% text(0.01, 0.65, ['a = 2.2: ', num2str(round(exportFlux(13)))],'Units','normalized')
+% text(0.01, 0.6, ['a = 2.3: ', num2str(round(exportFlux(14)))],'Units','normalized')
+xlabel('Particle Diameter [cm]')
+ylabel('\mu g C m^{-2} d^{-1}\mu m^{-1}') 
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+%legend('a = 1.6','a = 1.7','a = 1.8','a = 1.9','a = 2.0','a = 2.1','a = 2.2','a = 2.3','location','NorthEast')
+set(gca,'FontSize',12)
+
+nexttile
+
+loglog(2*x5*1E-4,NNN(9:11,:), 'LineWidth',1.5)
+hold on
+loglog(2*x4*1E-4,NNN(12:16,:), 'LineWidth',1.5)
+plot(2*x4*1E-4,slope(2*x4*1E-4,2),'b--','LineWidth',1.5)
+plot(2*x4*1E-4,slope(2*x4*1E-4,4)*100,'k--','LineWidth',1.5)
+%title('Particle size spectrum')
+xlabel('Particle Diameter [cm]')
+ylabel('Number spectrum [# cm^{-4}]')
+xlim([2*min(x5)*1E-4 2*max(x5)*1E-4])
+xticks([1E-3 1E-2 1E-1 1])
+legend('a = 1.6','a = 1.7','a = 1.8','a = 1.9','a = 2.0','a = 2.1','a = 2.2','a = 2.3','slope=-2','slope=-4','Location','EastOutside')
+% set(gca,'FontSize',12, 'YaxisLocation','right')
+set(gca,'FontSize',12)
